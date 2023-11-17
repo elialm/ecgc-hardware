a1a89ffafef51ecd23c6a55a0f2833988a7699b3
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-11-17 10:31:18 +0100

    Fixed U10 excluded from F.Paste and F.Mask

--- CartGen4/fab/zip.sh
+++ CartGen4/fab/zip.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Remove old fab.zip
+if [ -f fab.zip ]; then
+    rm fab.zip
+fi
+
 # Use script in fab directory to zip all fabrication files
-zip fab.zip ./CartGen4-*
+zip fab.zip ./CartGen4-*.gbr ./CartGen4-*.drl
